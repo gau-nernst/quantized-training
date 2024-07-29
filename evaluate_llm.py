64ac4ad0f405362cbc8227d3654486498818ac11
Thien Tran <gau.nernst@yahoo.com.sg>
2024-07-30 07:12:47 +0800

    remove compile option from evaluate

--- evaluate_llm.py
+++ evaluate_llm.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     parser.add_argument("--model", default="Qwen/Qwen2-0.5B-Instruct")
     parser.add_argument("--checkpoint")
     parser.add_argument("--tasks", nargs="+", default=["gsm8k"])
-    parser.add_argument("--compile", action="store_true")
     parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--max_seq_len", type=int, default=2048)
     parser.add_argument("--debug", action="store_true")
@@ -30,9 +29,6 @@ if __name__ == "__main__":
         if "model" in state_dict:
             state_dict = state_dict["model"]
         model.load_state_dict(state_dict)
-
-    if args.compile:
-        model.compile(mode="max-autotune", fullgraph=True)
 
     result = lm_eval.simple_evaluate(
         model=HFLM(pretrained=model, tokenizer=tokenizer, batch_size=args.batch_size, max_length=args.max_seq_len),
