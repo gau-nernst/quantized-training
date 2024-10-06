5c52ba35145ebb09e0dd4e2bfdf27da7573af92e
Thien Tran <gau.nernst@yahoo.com.sg>
2024-10-06 23:16:52 +0800

    add scaled int8 conv2d

--- kernels/__init__.py
+++ kernels/__init__.py
@@ -1,3 +1,3 @@
 from .cutlass_mm import int4_mm, scaled_int4_mm
-from .triton_conv2d import _triton_conv2d, int8_conv2d
+from .triton_conv2d import _triton_conv2d, int8_conv2d, scaled_int8_conv2d
 from .triton_mm import _triton_mm, int8_mm, scaled_int8_mm
--- kernels/triton_conv2d.py
+++ kernels/triton_conv2d.py
@@ -102,10 +102,6 @@ def _conv2d_kernel(
     w_base = W_ptr + (group * stride_wc_out * GROUP_OUT_C + idx_y_c * stride_wc_out)[None, :]
     acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=ACC_DTYPE)
 
-    # Could be simplified, but slightly slower:
-    # for i in range(KERNEL_H):
-    #     for j in range(KERNEL_W):
-    #         for k in range(0, GROUP_IN_C, BLOCK_K):
     BLOCK_K_COUNT = (GROUP_IN_C + BLOCK_K - 1) // BLOCK_K
     for ijk in range(KERNEL_H * KERNEL_W * BLOCK_K_COUNT):
         k = (ijk % BLOCK_K_COUNT) * BLOCK_K
@@ -218,4 +214,183 @@ def int8_conv2d(X: Tensor, W: Tensor, stride: int | tuple[int, int] = 1, padding
     return lib_ops.conv2d(X, W, stride, padding)
 
 
-# TODO: int8 conv2d, scaled int8 conv2d
+# this is mostly codegen-ed from torch.compile
+# NOTE: caller should make sure that inputs are channels-last for best performance
+@triton.autotune(
+    configs,
+    key=["BATCH", "IN_C", "IN_H", "IN_W", "OUT_C", "OUT_H", "OUT_W"],
+)
+@triton.jit
+def _scaled_int8_conv2d_kernel(
+    X_ptr,
+    W_ptr,
+    channel_scale_ptr,  # shape (OUT_C,)
+    out_ptr,
+    # Tensor dimensions
+    BATCH: int,
+    IN_C: int,
+    IN_H: int,
+    IN_W: int,
+    OUT_C: int,
+    OUT_H: int,
+    OUT_W: int,
+    # Strides:
+    stride_xn: int,
+    stride_xc: int,
+    stride_xh: int,
+    stride_xw: int,
+    stride_wc_out: int,
+    stride_wc_in: int,
+    stride_wh: int,
+    stride_ww: int,
+    stride_outn: int,
+    stride_outc: int,
+    stride_outh: int,
+    stride_outw: int,
+    #
+    KERNEL_H: tl.constexpr,
+    KERNEL_W: tl.constexpr,
+    STRIDE_H: tl.constexpr,
+    STRIDE_W: tl.constexpr,
+    PADDING_H: tl.constexpr,
+    PADDING_W: tl.constexpr,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    BLOCK_K: tl.constexpr,
+):
+    nhw = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)
+    idx_y_w = nhw % OUT_W
+    nh = nhw // OUT_W
+    idx_y_h = nh % OUT_H
+    idx_n = nh // OUT_H
+    idx_y_c = tl.program_id(1) * BLOCK_N + tl.arange(0, BLOCK_N)
+
+    group = 0
+    GROUP_IN_C = IN_C
+    GROUP_OUT_C = OUT_C
+
+    x_base = X_ptr + (group * stride_xc * GROUP_IN_C + idx_n * stride_xn)[:, None]
+    w_base = W_ptr + (group * stride_wc_out * GROUP_OUT_C + idx_y_c * stride_wc_out)[None, :]
+    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype=tl.int32)
+
+    BLOCK_K_COUNT = (GROUP_IN_C + BLOCK_K - 1) // BLOCK_K
+    for ijk in range(KERNEL_H * KERNEL_W * BLOCK_K_COUNT):
+        k = (ijk % BLOCK_K_COUNT) * BLOCK_K
+        ij = ijk // BLOCK_K_COUNT
+        i = ij // KERNEL_W
+        j = ij % KERNEL_W
+
+        idx_x_h = i - PADDING_H + idx_y_h * STRIDE_H
+        idx_x_w = j - PADDING_W + idx_y_w * STRIDE_W
+        idx_x_c = tl.arange(0, BLOCK_K) + k
+
+        x_ptrs = x_base + (
+            (idx_x_h * stride_xh)[:, None] + (idx_x_w * stride_xw)[:, None] + (idx_x_c * stride_xc)[None, :]
+        )
+        mask_x = (
+            (idx_n < BATCH)[:, None]
+            & (idx_x_h >= 0)[:, None]
+            & (idx_x_h < IN_H)[:, None]
+            & (idx_x_w >= 0)[:, None]
+            & (idx_x_w < IN_W)[:, None]
+            & (idx_x_c < GROUP_IN_C)[None, :]
+        )
+        matrix_x = tl.load(x_ptrs, mask=mask_x, other=0.0)
+
+        w_ptrs = w_base + ((idx_x_c * stride_wc_in)[:, None] + (i * stride_wh) + (j * stride_ww))
+        mask_w = (idx_x_c[:, None] < GROUP_IN_C) & (idx_y_c[None, :] < GROUP_OUT_C)
+        matrix_w = tl.load(w_ptrs, mask=mask_w, other=0.0)
+        acc += tl.dot(matrix_x, matrix_w)
+
+    mask = (
+        (idx_n < BATCH)[:, None]
+        & (idx_y_h < OUT_H)[:, None]
+        & (idx_y_w < OUT_W)[:, None]
+        & (idx_y_c < GROUP_OUT_C)[None, :]
+    )
+    idx_n = idx_n[:, None]
+    idx_c = idx_y_c[None, :] + group * GROUP_OUT_C
+    idx_h = idx_y_h[:, None]
+    idx_w = idx_y_w[:, None]
+
+    # new epilogue here
+    # TODO: add per-sample X_scale
+    channel_scale = tl.load(channel_scale_ptr + idx_c).to(tl.float32)
+    acc = acc.to(tl.float32) * channel_scale
+
+    out_ptr_offsets = tl.broadcast_to(
+        idx_n * stride_outn + idx_c * stride_outc + idx_h * stride_outh + idx_w * stride_outw, acc.shape
+    )
+    tl.store(out_ptr + out_ptr_offsets, acc, mask)
+
+
+lib.define("scaled_int8_conv2d(Tensor X, Tensor W, Tensor channel_scale, int[2] stride, int[2] padding) -> Tensor")
+
+
+@torch.library.impl(lib, "scaled_int8_conv2d", "CUDA")
+def _triton_scaled_int8_conv2d(
+    X: Tensor,
+    W: Tensor,
+    channel_scale: Tensor,
+    stride: tuple[int, int] = (1, 1),
+    padding: tuple[int, int] = (0, 0),
+) -> None:
+    BATCH, IN_C, IN_H, IN_W = X.shape
+    OUT_C, _, KERNEL_H, KERNEL_W = W.shape
+
+    # refer to https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html for equation
+    OUT_H = (IN_H + 2 * padding[0] - KERNEL_H) // stride[0] + 1
+    OUT_W = (IN_W + 2 * padding[1] - KERNEL_W) // stride[1] + 1
+
+    out = torch.empty(
+        BATCH,
+        OUT_C,
+        OUT_H,
+        OUT_W,
+        device=X.device,
+        dtype=channel_scale.dtype,
+        memory_format=torch.channels_last,
+    )
+
+    def grid(meta):
+        return (
+            triton.cdiv(BATCH * OUT_H * OUT_W, meta["BLOCK_M"]),
+            triton.cdiv(OUT_C, meta["BLOCK_N"]),
+        )
+
+    _scaled_int8_conv2d_kernel[grid](
+        X,
+        W,
+        channel_scale,
+        out,
+        BATCH,
+        IN_C,
+        IN_H,
+        IN_W,
+        OUT_C,
+        OUT_H,
+        OUT_W,
+        *X.stride(),
+        *W.stride(),
+        *out.stride(),
+        KERNEL_H,
+        KERNEL_W,
+        *stride,
+        *padding,
+    )
+
+    return out
+
+
+def scaled_int8_conv2d(
+    X: Tensor,
+    W: Tensor,
+    channel_scale: Tensor,
+    stride: int | tuple[int, int] = 1,
+    padding: int | tuple[int, int] = 0,
+) -> None:
+    assert X.dtype == W.dtype == torch.int8
+    assert X.is_contiguous(memory_format=torch.channels_last)
+    assert W.is_contiguous(memory_format=torch.channels_last)
+    assert channel_scale.is_contiguous()
+    return lib_ops.scaled_int8_conv2d(X, W, channel_scale, stride, padding)
